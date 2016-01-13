5b71ac4232eb97f056ecfb2bb9e4b3dc5a1712cf
Ole-André Rodlie <ole-andre.rodlie@inria.fr>
2016-01-13 16:23:50 +0100

    filter bas name

--- build-ffmpeg.sh
+++ build-ffmpeg.sh
@@ -29,7 +29,7 @@ WAVEPACK_TAR=wavpack-4.75.0.tar.bz2
 FFMPEG_TAR=ffmpeg-2.8.4.tar.xz
 #-------------------------------------------------
 
-FFMPEG_BASE_NAME=$(echo $FFMPEG_TAR | sed 's/.tar.bz2//')
+FFMPEG_BASE_NAME=$(echo $FFMPEG_TAR | sed 's/.tar.bz2//;s/.tar.gz//;s/.tar.xz//')
 
 if [ "$1" == "32" ]; then
 	ARCH=i686
